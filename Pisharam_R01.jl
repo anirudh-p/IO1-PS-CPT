ecd71566be30fd27a7bc23a6b8d40f4d0284d58f
anirudh-p <30889924+anirudh-p@users.noreply.github.com>
2022-09-05 20:26:43 -0400

    Create Pisharam_R01.jl

---
+++ Pisharam_R01.jl
@@ -0,0 +1,16 @@
+#Step 1.1.1: Fix Parameter Values
+
+β_1 = 0.2
+β_2 = 0.2
+μ = 0
+σ^D = 1
+σ^S = 1
+
+#Step 1.1.2: Generate Realizations of Shocks
+
+#Step 1.1.3: Generate Price Data
+
+#Step 1.1.4: Use Price and Equilibrium Conditions to generate (P,Q)
+
+#Step 1.1.5: Plot Empirical Distributions of P and Q
+
