a7a3d0faca4197c41ff778ab5450a50e281c2165
anirudh-p <30889924+anirudh-p@users.noreply.github.com>
2022-09-05 22:09:33 -0400

    Update Pisharam_R01.jl

--- PS0/Pisharam_R01.jl
+++ PS0/Pisharam_R01.jl
@@ -1,14 +1,17 @@
 #Step 1.1.1: Fix Parameter Values
-
-β_1 = 0.2
-β_2 = 0.2
+β_1 = 0
+β_2 = 2
+δ = 0.2
 μ = 0
-σ^D = 1
+σ^D = 1 
 σ^S = 1
 
 #Step 1.1.2: Generate Realizations of Shocks
+ϵ_D = Array
+a = Array
 
 #Step 1.1.3: Generate Price Data
+ln(P) = (1/(1+β_2*δ))*(δ*β_1 + δ*ϵ_D + ln(μ) - ln(a))
 
 #Step 1.1.4: Use Price and Equilibrium Conditions to generate (P,Q)
 
