06ef5ee3f57639b2fd9c9eab524840eaa194cf8d
anirudh-p <30889924+anirudh-p@users.noreply.github.com>
2022-09-07 11:31:25 -0400

    Update 9/5
    Updated Parameter values and reduced form for price equation

--- PS0/Pisharam_R01.jl
+++ PS0/Pisharam_R01.jl
@@ -1,19 +1,41 @@
+#Step 0: Import Packages
+    #import Pkg;
+    #using Pkg;
+    #Pkg.add("Distributions") // Importing the Distributions Package
+    #Pkg.add("Plots")         // Importing the Plot Package
+    
+
 #Step 1.1.1: Fix Parameter Values
 β_1 = 0
 β_2 = 2
 δ = 0.2
-μ = 0
-σ^D = 1 
-σ^S = 1
+μ = 1 #Mistake in the Problem Set?
+s_D = 1 
+s_S = 1
 
 #Step 1.1.2: Generate Realizations of Shocks
-ϵ_D = Array
-a = Array
+using Random, Distributions
+Random.seed!(123)   
+
+d_1 = Normal(0,s_D)
+d_2 = Normal(0,s_S)
+
+ϵ_D = rand(d_1,50)
+ϵ_a = rand(d_2,50)
 
 #Step 1.1.3: Generate Price Data
-ln(P) = (1/(1+β_2*δ))*(δ*β_1 + δ*ϵ_D + ln(μ) - ln(a))
+ln_P = (1/(1+β_2*δ))*(δ*β_1 .+ δ*ϵ_D .+ log(μ) .- ϵ_a)
+P = exp.(ln_P)
 
 #Step 1.1.4: Use Price and Equilibrium Conditions to generate (P,Q)
+ln_Q = β_1 .- β_2*ln_P .+ ϵ_D
+Q = exp.(ln_Q)
 
 #Step 1.1.5: Plot Empirical Distributions of P and Q
+using Plots
+y=P; x=Q;
+plot(x, y, seriestype = :scatter, title = "P vs Q")
 
+#Step 1.2.1: OLS Regression
+
+#Step 1.2.2: Method of Moments
