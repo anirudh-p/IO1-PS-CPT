d074f7677d97642a612faa5bd23bdcf5d5bccdd5
kchoi2 <kevin.choi.2@bc.edu>
2022-09-06 22:44:54 -0400

    Code up to 1.2.1.1

--- PS0/Pisharam_R01.jl
+++ PS0/Pisharam_R01.jl
@@ -1,19 +1,37 @@
+import Pkg; Pkg.add("Distributions")
+import Pkg; Pkg.add("Plots")
+import Pkg; Pkg.add("GLM")
+using Distributions
+using Plots
+using GLM
+using DataFrames
+
 #Step 1.1.1: Fix Parameter Values
 β_1 = 0
 β_2 = 2
 δ = 0.2
 μ = 0
-σ^D = 1 
-σ^S = 1
+σ_D = 1 
+σ_S = 1
 
 #Step 1.1.2: Generate Realizations of Shocks
-ϵ_D = Array
-a = Array
+ϵ_D = rand(Normal(0,σ_D),50)
+a = rand(Normal(0,σ_S), 50)
 
 #Step 1.1.3: Generate Price Data
-ln(P) = (1/(1+β_2*δ))*(δ*β_1 + δ*ϵ_D + ln(μ) - ln(a))
+logP = (1/(1+β_2*δ))*(δ*ϵ_D - a)
 
 #Step 1.1.4: Use Price and Equilibrium Conditions to generate (P,Q)
+logQ = β_2*logP + ϵ_D
 
 #Step 1.1.5: Plot Empirical Distributions of P and Q
 
+plot(logQ, logP, seriestype = :scatter, title = "Log Price and Quantity", xlabel = "Log Quantity", ylabel = "Log Price")
+
+
+
+#Step 1.2.1.1: Report OLS Estimate of β_2
+simulated = DataFrame(logQ = logQ, logP = logP)
+ols = lm(@formula(logP ~ logQ), simulated)
+
+#Step 1.2.2.1: MoM
