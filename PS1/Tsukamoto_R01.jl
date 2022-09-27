8adbe745a4878239679d05b5497fb32143bf03ce
anirudh-p <30889924+anirudh-p@users.noreply.github.com>
2022-09-27 17:11:00 -0400

    Part 0

--- PS1/Tsukamoto_R01.jl
+++ PS1/Tsukamoto_R01.jl
@@ -1,4 +1,6 @@
-using Random, Distributions, LinearAlgebra 
+using Random
+using Distributions
+using LinearAlgebra 
 
 #********************
 #Step0 Simulate Data
@@ -9,51 +11,29 @@ d_2 = Normal(0,1)
 
 #0.1: Product Characteristics
 
-#Firm j =1
-X_21 = rand(d_1,100) #Uniform [0,1]
-X_31 = rand(d_2,100) #Normal [0,1]
-X_1 = zeros(100,3)
-for i in 1:100
-    X_1[i,1] = 1
-    X_1[i,2] = X_21[i]
-    X_1[i,3] = X_31[i]
-end
+#X is a tensor of product Characteristics across market*Characteristic*firm
 
-#Firm j =2
-X_22 = rand(d_1,100)
-X_32 = rand(d_2,100)
-X_2 = zeros(100,3)
-for i in 1:100
-    X_1[i,1] = 1
-    X_1[i,2] = X_21[i]
-    X_1[i,3] = X_31[i]
-end
+x_2 = rand(d_1,300) #Uniform [0,1]
+x_3 = rand(d_2,300) #Normal(0,1)
+X = zeros(100,3,3)
 
-#Firm j =3
-X_23 = rand(d_1,100)
-X_33 = rand(d_2,100)
-X_3 = zeros(100,3)
-for i in 1:100
-    X_1[i,1] = 1
-    X_1[i,2] = X_21[i]
-    X_1[i,3] = X_31[i]
+k=0
+for j = 1:3
+    for i=1:100
+        X[i,1,j] = 1
+        X[i,2,j] = x_2[k+i]
+        X[i,3,j] = x_3[k+i]
+    end
+    k=j*100
 end
 
 #0.2: Demand Shock
-ξ_1 = rand(d_2,100)
-ξ_2 = rand(d_2,100) 
-ξ_3 = rand(d_2,100)
+ξ = hcat(last(rand(d_2,400),100),last(rand(d_2,500),100),last(rand(d_2,600),100)) #First 300 Draws used up for Product Characteristics (Seed Set)
 
 #0.3: Cost Shifters
-W_1 = rand(d_1,1)
-W_2 = rand(d_1,1)
-W_3 = rand(d_1,1)
-Z_1 = rand(d_2,100)
-Z_2 = rand(d_2,100)
-Z_3 = rand(d_2,100)
-η_1 = rand(d_1,100)
-η_2 = rand(d_2,100)
-η_3 = rand(d_3,100)
+W = hcat(last(rand(d_2,700),100),last(rand(d_2,800),100),last(rand(d_2,900),100)) #First 600 Draws used up (Seed Set)
+Z = hcat(last(rand(d_2,1000),100),last(rand(d_2,1100),100),last(rand(d_2,1200),100)) 
+η = hcat(last(rand(d_2,1300),100),last(rand(d_2,1400),100),last(rand(d_2,1500),100)) 
 
 #4.Fix parameter values
 β = [5,1,1]
@@ -61,12 +41,21 @@ Z_3 = rand(d_2,100)
 σ_α = 1
 γ = [2,1,1]
 
-#5.Derivation of prices and market shares
-δ_1 = X_1*β - α* #Mean Utilities
+#5.Derivation of prices and market share
+#Simulate α_i's
+d_3 = LogNormal(0,1)
+temp = rand(d_3,100000)
+α_i = α .+ temp.*σ_α
 
-MC_1 = hcat(ones(100), W_1, Z_1)*γ + η_1
-MC_2 = hcat(ones(100), W_2, Z_2)*γ + η_2
-MC_3 = hcat(ones(100), W_3, Z_3)*γ + η_3
+p = zeros(100,3)
 
-ln(s_1) - ln(s_0) = δ_1
-(p_1 - MC_1)/p_1 = 
+δ_0 = 0
+δ_1 = X[:,:,1]*β -α*p[:,1] + ξ[:,1]
+δ_2 = X[:,:,2]*β -α*p[:,2] + ξ[:,2]
+δ_3 = X[:,:,3]*β -α*p[:,3] + ξ[:,3]
+
+s_1 = s_0*exp(δ_1 - δ_0)
+s_3 = s_0*exp(δ_2 - δ_0)
+s_2 = s_0*exp(δ_3 - δ_0)
+
+δ_0 = X
