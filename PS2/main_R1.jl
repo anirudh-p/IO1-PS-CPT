926482a3e21c6836cb7834f647f726eac4278d8a
kchoi2 <kevin.choi.2@bc.edu>
2022-11-05 15:33:34 -0400

    calc_mi_subsample

--- PS2/main_R1.jl
+++ PS2/main_R1.jl
@@ -94,23 +94,24 @@ gmm_berry = optimize(obj_berry, lower, upper, initial)
 
 function sim_tamer(μ, σ, T, data)
     entryData = data
+    s = size(entryData)[1]
     (α,β,δ) = (1,1,1)
     d1 = Normal(μ,σ)
-    h1_hat = zeros(100,3,100)
-    h2_hat = zeros(100,3,100)
+    h1_hat = zeros(s,3,T)
+    h2_hat = zeros(s,3,T)
     for t in 1:T
-        u = rand(d1,100,3)
+        u = rand(d1,s,3)
 
-        Φ = zeros(100,3)
+        Φ = zeros(s,3)
 
         Φ = α*Matrix(entryData[:,2:4]) + u 
 
 
         #Calculate profits
         Π = zeros(8,3)
-        high = zeros(100, 3)
-        low = zeros(100, 3)
-        for m in 1:100
+        high = zeros(s, 3)
+        low = zeros(s, 3)
+        for m in 1:s
             Π[1,1] = β*entryData.X[m] - δ*log(3) - Φ[m,1]
             Π[1,2] = β*entryData.X[m] - δ*log(3) - Φ[m,2]
             Π[1,3] = β*entryData.X[m] - δ*log(3) - Φ[m,3]
@@ -190,4 +191,62 @@ function calc_mi(μ, data)
     return Q
 end
 
-#calc_mi(1, entryData)
+calc_mi(1, entryData)
+# Let min_mi be the minimized value of the objective calc_mi(μ; data) = a_n Q_n(μ, data)
+
+res = optimize(μ -> calc_mi(μ, entryData), .5, 4)
+min_mi = Optim.minimum(res)
+
+# Define c0 as the 1.25*min_mi following Ciliberto-Tamer
+
+c0 = 1.25 * min_mi
+
+# Find initial confidence region by evaluating the obj function in a grid
+# of 50 points (from -1 to 4.0)
+#calc_mi = μ -> calc_mi(μ, entryData)
+MU = -1:0.1:4 
+MU_I = zeros(51)
+for i in 1:51
+    MU_I[i] = calc_mi(MU[i],entryData)
+end
+MU_I = MU_I[MU_I .<= c0] 
+μ0_lb = minimum(MU_I)
+μ0_ub = maximum(MU_I)
+
+
+# Generate subsamples with a subsample size of M/4 following Ciliberto-Tamer
+# Compute max value of the obj function of each subsample over initial 
+# confidence region by finding the min of the negative obj function, and
+# subtract from that the min of the obj function in the same region
+# following Ciliberto-Tamer to correct for potential misspecification.
+
+M = 100
+B = 100
+
+# Write a function subsample(data, size) to generate subsamples from data of a particular size
+
+
+function calc_mi_subsample(MU_I, data, M)
+    m = Int64(M/4)
+    sub_data = sample(1:M, m, replace =false)
+    obj_values = map(μ -> calc_mi(μ, data[sub_data,:]), MU_I) #Calculate a_n Q_n(μ, sub_data) for all μ in μ_I
+    max_mi_sub = maximum(obj_values) # C_n = sup_{μ ∈ μ_I} a_n Q_n(μ, sub_data)
+    min_mi_sub = minimum(obj_values) # to correct for misspecification
+    return max_mi_sub - min_mi_sub 
+end
+
+# Take 1/4 the 95th percentile and set equal to c1 to compute 95% CI (1/4
+# because #subsample=M/4)
+
+c1_subsamples = map(calc_mi_subsample(MU_I, entryData, M), 1:B)
+c1 = 1/4 * quantile(c1_subsamples, 0.95)
+
+# compute ci1 using Ciliberto and Tamer's estimator modified for
+# misspecification
+MU_I = MU[calc_mi.(MU) - min_mi  .<= c1]
+μ1_lb = minimum(MU_I)
+μ1_ub = maximum(MU_I)
+
+#Last, repeat the subsampling procedure a few times to update the bounds
+#further. In doing so, use a finer grid to obtain more accurate bounds (e.g.,
+#MU = -1:0.025:4).
